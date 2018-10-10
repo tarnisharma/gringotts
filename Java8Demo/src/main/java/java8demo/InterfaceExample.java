b741d579ae146a42c38c7b464c5ebe48e023fe46
Tarni Sharma <tarni.sharma@gmail.com>
2018-10-11 00:22:24 +0530

    default method in interface in java8

--- Java8Demo/src/main/java/java8demo/InterfaceExample.java
+++ Java8Demo/src/main/java/java8demo/InterfaceExample.java
@@ -3,33 +3,49 @@ package java8demo;
 interface Y {
 	void print();
 	void y();
+	default void hello() {
+		System.out.println("Hello Y");
+	}
 }
 
 interface X {
 	void print();
 	void x();
+	default void hello() {
+		System.out.println("Hello X");
+	}
 }
 
-
-
-public class InterfaceExample implements X,Y{
+public class InterfaceExample implements X, Y {
 
 	@Override
 	public void y() {
 		System.out.println("Y");
-		
+
 	}
 
 	@Override
 	public void x() {
 		System.out.println("X");
-		
+
 	}
 
 	@Override
 	public void print() {
 		System.out.println("Hello but not sure if its the implementation of X or Y ");
-		
+
 	}
 
+	@Override
+	public void hello() {
+		System.out.println("Overriding  Hello");
+	}
+
+	public static void main(String args[]) {
+		InterfaceExample obj = new InterfaceExample();
+		obj.x();
+		obj.y();
+		obj.hello();
+		obj.print();
+	}
 }
